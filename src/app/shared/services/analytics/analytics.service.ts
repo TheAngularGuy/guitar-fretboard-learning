5cb233070479733e361e5e0f801cd92c0fe4e584
Mustapha Aouas <mustapha.aouas@epitech.eu>
2019-12-09 17:07:33 +0100

    adding version to analytics

--- src/app/shared/services/analytics/analytics.service.ts
+++ src/app/shared/services/analytics/analytics.service.ts
@@ -1,11 +1,12 @@
 import * as firebase from 'firebase/app';
+import { environment } from 'src/environments/environment';
 
 export class AnalyticsService {
   private analitics: firebase.analytics.Analytics;
 
   init() {
     this.analitics = firebase.analytics();
-    this.analitics.logEvent('Initialized app', { appAnalyticsInit: true });
+    this.analitics.logEvent('Initialized app', { appVersion: environment.version });
   }
 
   getAnalitics() {
