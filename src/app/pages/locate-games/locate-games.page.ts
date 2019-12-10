f4041b64cbe46887217e6a37f15d757be626d1ee
Mustapha Aouas <mustapha.aouas@epitech.eu>
2019-12-10 18:06:16 +0100

    list mobile friendly

--- src/app/pages/locate-games/locate-games.page.ts
+++ src/app/pages/locate-games/locate-games.page.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { listAnimation } from 'src/app/animations/list.animation';
 
 @Component({
   selector: 'app-locate-games',
   templateUrl: './locate-games.page.html',
   styleUrls: ['./locate-games.page.scss'],
+  animations: [listAnimation],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LocateGamesPage implements OnInit {
@@ -20,6 +22,12 @@ export class LocateGamesPage implements OnInit {
       name: 'Locate all',
       text:
         'In this game you have to locate all the instances of a note in the fretboard and click on them.',
+    },
+    {
+      img: 'assets/imgs/megaphone.svg',
+      name: 'More games soon',
+      text: `More game modes are coming soon. If you like this app please share it with your
+        friends or consider buying me a cofee by <a href="https://www.buymeacoffee.com/fiywQH2Dc">clicking here</a>.`,
     },
   ];
 
