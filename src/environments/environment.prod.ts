0b8d32a9fb537b339f8266c319881e0c0ea93e6e
Mustapha Aouas <mustapha.aouas@epitech.eu>
2019-12-20 11:17:09 +0100

    --

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  version: '1.4.0',
+  version: '1.4.1',
   firebaseConfig: {
     apiKey: 'AIzaSyC-3m7qdeMizksexLgt2pAA9SD6BGMA3CQ',
     authDomain: 'guitar-fretboard-learning.firebaseapp.com',
